2fcb62159c58ca5fff3e0960a41533cc694ccc0f
arastogi603 <arastogi603@gmail.com>
2026-07-24 22:42:33 +0530

    Cleanup: removed unused images, media files, and redundant UI components

--- frontend/extract.js
+++
@@ -1,8 +0,0 @@
-fetch('https://my.spline.design/robotfollowcursorforlandingpage-6onQnqC4nU4Kjlquc7tKpYrg/')
-  .then(res => res.text())
-  .then(text => {
-    const match = text.match(/https:\/\/[^"']*\.splinecode/);
-    if (match) console.log(match[0]);
-    else console.log("Not found");
-  })
-  .catch(err => console.error(err));
